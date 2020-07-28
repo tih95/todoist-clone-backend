cd98daae5b8a6d0266988950e202d1c7dc7df047
tih95 <tristan.honda@gmail.com>
2020-07-27 23:25:32 -0700

    small refactor

--- routes/projects.js
+++ routes/projects.js
@@ -3,114 +3,121 @@ const pool = require('../db');
 const authorizeToken = require('../middleware/authorizeToken');
 
 projectRouter.get('/', authorizeToken, async (req, res) => {
-  const user = req.user;
-  console.log('is there a user backend?', user);
-  try {
-    const result = await pool.query(`
+	const user = req.user;
+	console.log('is there a user backend?', user);
+	try {
+		const result = await pool.query(
+			`
       SELECT * FROM projects
       WHERE u_id = $1
-    `, [user.id])
+    `,
+			[ user.id ]
+		);
 
-    const projects = result.rows;
+		const projects = result.rows;
 
-    return res.json(projects);
-  }
-  catch(e) {
-    console.log('error', e);
-    return res.status(400).json(e);
-  }
-})
+		return res.json(projects);
+	} catch (e) {
+		console.log('error', e);
+		return res.status(400).json(e);
+	}
+});
 
 projectRouter.post('/', authorizeToken, async (req, res) => {
-  const body = req.body;
-  const user = req.user;
+	const body = req.body;
+	const user = req.user;
 
-  try {
-    const result = await pool.query(`
+	try {
+		const result = await pool.query(
+			`
       INSERT INTO projects
       (name, u_id, color)
       VALUES ($1, $2, $3)
       RETURNING *
-    `, [body.name, user.id, body.color])
+    `,
+			[ body.name, user.id, body.color ]
+		);
 
-    const createdProject = result.rows[0];
-    console.log('createdProject', createdProject);
-    res.status(200).json(createdProject);
-  }
-  catch(e) {
-    console.log('got here');
-    return res.status(400).json({ error: e });
-  }
+		const createdProject = result.rows[0];
+		console.log('createdProject', createdProject);
+		res.status(200).json(createdProject);
+	} catch (e) {
+		console.log('got here');
+		return res.status(400).json({ error: e });
+	}
 });
 
 projectRouter.post('/:id/addTodo', authorizeToken, async (req, res) => {
-  const { user, body } = req;
-  const { id } = req.params;
-  let due_date;
+	const { user, body } = req;
+	const { id } = req.params;
+	let due_date;
 
-  if (!body.due_date) {
-    due_date = null;
-  }
+	if (!body.due_date) {
+		due_date = null;
+	}
 
-  try {
-    const result = pool.query(`
+	try {
+		const result = pool.query(
+			`
       INSERT INTO todos
       (task, completed, priority, u_id, due_date, p_id)
       VALUES
       ($1, $2, $3, $4, $5, $6)
       RETURNING *
-    `, [body.task, false, body.priority, user.id, due_date, id]);
+    `,
+			[ body.task, false, body.priority, user.id, due_date, id ]
+		);
 
-    const createdTodo = result.rows[0];
+		const createdTodo = result.rows[0];
 
-    res.status(200).json(createdTodo);
-  }
-  catch(e) {
-    res.status(400).json(e);
-  }
-})
+		res.status(200).json(createdTodo);
+	} catch (e) {
+		res.status(400).json(e);
+	}
+});
 
 projectRouter.put('/:id', authorizeToken, async (req, res) => {
-  const user = req.user;
-  const { id } = req.params;
-  const body = req.body;
-  console.log(id);
-  try {
-    const result = await pool.query(`
+	const { id } = req.params;
+	const body = req.body;
+
+	try {
+		const result = await pool.query(
+			`
       UPDATE projects 
       SET name=$1, color=$2
       WHERE p_id=$3
       RETURNING *
-    `, [body.name, body.color, id]);
+    `,
+			[ body.name, body.color, id ]
+		);
 
-    const editedTodo = result.rows[0];
+		const editedTodo = result.rows[0];
 
-    res.json(editedTodo);
-  }
-  catch(e) {
-    res.status(400).json({errMsg: 'something happend'});
-  }
-})
+		res.json(editedTodo);
+	} catch (e) {
+		res.status(400).json({ errMsg: 'something happend' });
+	}
+});
 
 projectRouter.delete('/:id', authorizeToken, async (req, res) => {
-  const user = req.user;
-  const { id } = req.params;
+	const { id } = req.params;
 
-  try {
-    const result = await pool.query(`
+	try {
+		const result = await pool.query(
+			`
     DELETE FROM projects
     WHERE p_id=$1
     RETURNING *
-  `, [id])
+  `,
+			[ id ]
+		);
 
-    const deletedTodo = result.rows[0];
+		const deletedTodo = result.rows[0];
 
-    res.json(deletedTodo);
-  }
-  catch(e) {
-    res.status(400).json({errMsg: 'something happened'})
-  }
-  
-})
+		res.json(deletedTodo);
+	} catch (e) {
+		res.status(400).json({ errMsg: 'something happened' });
+	}
+});
 
 module.exports = projectRouter;
--- routes/users.js
+++ routes/users.js
@@ -5,89 +5,97 @@ const config = require('../config');
 const pool = require('../db');
 
 userRouter.post('/register', async (req, res) => {
-  const body = req.body;
+	const body = req.body;
 
-  try {
-    
-    const user = await pool.query(`
+	try {
+		const user = await pool.query(
+			`
       SELECT * FROM users
       WHERE email = $1
-    `, [body.email])
+    `,
+			[ body.email ]
+		);
 
-    if (user.rows.length > 0) {
-      return res.status(401).json({ errMsg: 'Account already exists' });
-    }
+		if (user.rows.length > 0) {
+			return res.status(401).json({ errMsg: 'Account already exists' });
+		}
 
-    const saltRounds = 10;
-    const passwordHash = await bcrypt.hash(body.password, saltRounds);
+		const saltRounds = 10;
+		const passwordHash = await bcrypt.hash(body.password, saltRounds);
 
-    // begin transaction of creating a user and a default Inbox project
-    await pool.query('BEGIN');
+		// begin transaction of creating a user and a default Inbox project
+		await pool.query('BEGIN');
 
-    const result = await pool.query(`
+		const result = await pool.query(
+			`
       INSERT INTO users
       (name, email, password_hash)
       VALUES ($1, $2, $3)
       RETURNING *
-    `, [body.name, body.email, passwordHash])
+    `,
+			[ body.name, body.email, passwordHash ]
+		);
 
-    const retUser = result.rows[0];
+		const retUser = result.rows[0];
 
-    // automatically creates a new project 'inbox'
-    await pool.query(`
+		// automatically creates a new project 'inbox'
+		await pool.query(
+			`
     INSERT INTO projects
     (name, u_id)
     VALUES ('inbox', $1)
-  `, [retUser.u_id]);
+  `,
+			[ retUser.u_id ]
+		);
 
-    await pool.query('COMMIT');
-    console.log('got here');
-    const userForToken = {
-      id: retUser.u_id
-    }
+		await pool.query('COMMIT');
+		console.log('got here');
+		const userForToken = {
+			id: retUser.u_id
+		};
 
-    const token = jwt.sign(userForToken, config.JWT_SECRET);
+		const token = jwt.sign(userForToken, config.JWT_SECRET);
 
-    res.status(200).json({ token, name: retUser.name, email: retUser.email });
-  }
-  catch(err) {
-    res.status(500).send({ err })
-  }
-})
+		res.status(200).json({ token, name: retUser.name, email: retUser.email });
+	} catch (err) {
+		res.status(500).send({ err });
+	}
+});
 
 userRouter.post('/login', async (req, res) => {
-  const body = req.body;
+	const body = req.body;
 
-  try {
-    const result = await pool.query(`
+	try {
+		const result = await pool.query(
+			`
       SELECT * FROM users
       WHERE email = $1
-    `, [body.email]);
+    `,
+			[ body.email ]
+		);
 
-    if (result.rows.length === 0) {
-      return res.status(401).json({ errMsg: 'User account does not exist' });
-    }
+		if (result.rows.length === 0) {
+			return res.status(401).json({ errMsg: 'User account does not exist' });
+		}
 
-    const loggedInUser = result.rows[0];
-    const passwordCorrect = await bcrypt.compare(body.password, loggedInUser.password_hash);
+		const loggedInUser = result.rows[0];
+		const passwordCorrect = await bcrypt.compare(body.password, loggedInUser.password_hash);
 
-    if (passwordCorrect) {
-      console.log('loggedInUser');
-      const userForToken = {
-        id: loggedInUser.u_id
-      }
+		if (passwordCorrect) {
+			const userForToken = {
+				id: loggedInUser.u_id
+			};
 
-      const token = jwt.sign(userForToken, config.JWT_SECRET);
+			const token = jwt.sign(userForToken, config.JWT_SECRET);
 
-      res.status(200).json({ token, name: loggedInUser.name, email: loggedInUser.email });
-    }
-    else {
-      return res.status(400).json({ errMsg: 'Password is incorrect' })
-    }
-  }
-  catch(err) {
-    res.status(500).json({ err })
-  }
-})
+			res.status(200).json({ token, name: loggedInUser.name, email: loggedInUser.email });
+		}
+		else {
+			return res.status(400).json({ errMsg: 'Password is incorrect' });
+		}
+	} catch (err) {
+		res.status(500).json({ err });
+	}
+});
 
 module.exports = userRouter;
